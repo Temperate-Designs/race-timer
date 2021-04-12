e7e83550bfa818754c9b3472a05ef96e948eca1e
Nicolas Bock <nicolasbock@gmail.com>
2021-04-12 11:09:55 -0600

    Add a stop watch
    This change adds the stop watch functionality.
    
    Signed-off-by: Nicolas Bock <nicolasbock@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -27,8 +30,50 @@ class RaceHomePage extends StatefulWidget {
   _RaceHomePageState createState() => _RaceHomePageState();
 }
 
+class Racer {
+  int bibNumber = -1;
+  String name = "";
+  double time = 0.0;
+
+  Racer(this.bibNumber, this.name);
+}
+
 class _RaceHomePageState extends State<RaceHomePage> {
-  List<int> bibNumbers = [];
+  List<Racer> racers = [];
+  Stopwatch _stopwatch = new Stopwatch();
+  late Timer _timer;
+
+  @override
+  void initState() {
+    super.initState();
+    _timer = new Timer.periodic(new Duration(milliseconds: 30), (timer) {
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
+  }
+
+  void handleStartStop() {
+    if (_stopwatch.isRunning) {
+      _stopwatch.stop();
+    } else {
+      _stopwatch.start();
+    }
+    setState(() {}); // re-render the page
+  }
+
+  String formatTime(int milliseconds) {
+    var secs = milliseconds ~/ 1000;
+    var hundredths = (milliseconds % 100).toString().padLeft(2, '0');
+    var hours = (secs ~/ 3600).toString().padLeft(2, '0');
+    var minutes = ((secs % 3600) ~/ 60).toString().padLeft(2, '0');
+    var seconds = (secs % 60).toString().padLeft(2, '0');
+    return "$hours:$minutes:$seconds:$hundredths";
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -40,20 +85,48 @@ class _RaceHomePageState extends State<RaceHomePage> {
           child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
+          Row(
+            children: [
+              Container(
+                  margin: const EdgeInsets.all(16),
+                  padding: const EdgeInsets.all(10),
+                  decoration:
+                      BoxDecoration(color: CupertinoColors.lightBackgroundGray),
+                  child: Text(formatTime(_stopwatch.elapsedMilliseconds),
+                      style: TextStyle(
+                          fontSize: 42, fontWeight: FontWeight.bold))),
+              Align(
+                alignment: Alignment.centerRight,
+                child: FloatingActionButton(
+                    tooltip: 'Start Race',
+                    child: _stopwatch.isRunning
+                        ? Icon(Icons.stop_outlined)
+                        : Icon(Icons.play_arrow_outlined),
+                    onPressed: handleStartStop),
+              ),
+            ],
+          ),
           Container(
-              margin: const EdgeInsets.all(16.0),
+              margin: const EdgeInsets.all(6.0),
               padding: const EdgeInsets.all(6.0),
-              decoration:
-                  BoxDecoration(border: Border.all(color: Colors.blueAccent)),
-              child: Text('Racers')),
+              decoration: BoxDecoration(
+                  color: Colors.lightBlue,
+                  border: Border.all(color: Colors.blueAccent)),
+              child: Text('Racers',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold))),
           Expanded(
             child: ListView.builder(
-              itemCount: bibNumbers.length,
+              itemCount: racers.length,
               itemBuilder: (context, index) {
                 return ListTile(
-                  title: Center(
-                    child: Text(index.toString().padLeft(4, '0')),
-                  ),
+                  title: Text(
+                      'Bib: ${racers[index].bibNumber.toString().padLeft(4, '0')}'),
+                  subtitle: Text(racers[index].name),
+                  trailing: Icon(Icons.alarm_off),
+                  dense: true,
                 );
               },
             ),
@@ -67,7 +140,7 @@ class _RaceHomePageState extends State<RaceHomePage> {
       floatingActionButton: FloatingActionButton.extended(
         onPressed: () {
           setState(() {
-            bibNumbers.add(bibNumbers.length + 1);
+            racers.add(new Racer(racers.length + 1, ""));
           });
         },
         tooltip: 'Add Racer',
