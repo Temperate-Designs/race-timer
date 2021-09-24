a98fbe859a0c8ab46a4eb3d41a043f5fc4a2afe2
Nicolas Bock <nicolasbock@gmail.com>
2021-09-24 09:48:28 -0600

    Add localization support
    Signed-off-by: Nicolas Bock <nicolasbock@gmail.com>

--- lib/race_app.dart
+++ lib/race_app.dart
@@ -3,6 +3,8 @@ import 'dart:async';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 import "add_racer.dart";
 
@@ -15,6 +17,17 @@ class RaceApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: title,
+      localizationsDelegates: const [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('en', ''), // English, no country code
+      ],
+      onGenerateTitle: (BuildContext context) =>
+          AppLocalizations.of(context)!.title,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
