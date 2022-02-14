797ac5e10f85c14c14d59071ad733a590e66e7e1
Nicolas Bock <nicolasbock@gmail.com>
2022-02-14 09:44:17 -0700

    Convert to `StatefulWidget`
    Signed-off-by: Nicolas Bock <nicolasbock@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -11,12 +11,17 @@ class Racer {
 }
 
 void main() {
-  runApp(const RaceTimer());
+  runApp(const RaceTimerWidget());
 }
 
-class RaceTimer extends StatelessWidget {
-  const RaceTimer({Key? key}) : super(key: key);
+class RaceTimerWidget extends StatefulWidget {
+  const RaceTimerWidget({Key? key}) : super(key: key);
 
+  @override
+  _RaceTimerState createState() => _RaceTimerState();
+}
+
+class _RaceTimerState extends State<RaceTimerWidget> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
